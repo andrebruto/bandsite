bcc655185a472dddf415071ce12248386dc4b64c
André Bruto <andrebruto@MacBook-Pro-de-Andre.local>
2020-07-28 16:23:19 -0700

    first commit of sprint three

---
+++ sprint-three/scripts/build-bio-page.js
@@ -0,0 +1,103 @@
+const commentsSection = document.getElementById("comments");
+
+const comments = [
+  {
+    author: "Micheal Lyons",
+    date: new Date("12/18/2018"),
+    comment:
+      "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.",
+  },
+  {
+    author: "Gary Wong",
+    date: new Date("12/18/2018"),
+    comment:
+      "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!",
+  },
+  {
+    author: "Theodore Duncan",
+    date: new Date("11/15/2018"),
+    comment:
+      "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!",
+  },
+];
+
+function loopThroughComments(commentsArr) {
+  // sorts through comments dates
+  const sortedArry = commentsArr.sort(function (a, b) {
+    return b.date - a.date;
+  });
+
+  for (let i = 0; i < sortedArry.length; i++) {
+    // loops into the comments array
+    displayComment(commentsArr[i]); // calls the function with the loops to pass each object of the array
+  }
+}
+
+function displayComment(comment) {
+  // creates the div that will store each full comment structure
+  const commentsComment = document.createElement("div");
+  commentsComment.classList.add("comments__comment");
+  const commentsBlock = document.getElementById("comments-output");
+  commentsBlock.appendChild(commentsComment);
+
+  // creates the image of the author of the comment
+  const commentPhoto = document.createElement("img");
+  commentPhoto.classList.add("comments__comment-photo");
+  commentsComment.appendChild(commentPhoto);
+
+  // creates a div to store the full comment structure but the image
+  const commentFilledForm = document.createElement("div");
+  commentFilledForm.classList.add("comments__comment-filledForm");
+  commentsComment.appendChild(commentFilledForm);
+
+  // creates a div to store both the comment author and the comment date
+  const commentsCommentTitle = document.createElement("div");
+  commentsCommentTitle.classList.add("comments__comment-title");
+  commentFilledForm.appendChild(commentsCommentTitle);
+
+  // adds the author of the comment
+  const commentsCommentAuthor = document.createElement("h4");
+  commentsCommentAuthor.classList.add("comments__comment-author");
+  commentsCommentAuthor.innerText = comment.author;
+  commentsCommentTitle.appendChild(commentsCommentAuthor);
+
+  // adds the date of the comment
+  const commentsCommentDate = document.createElement("h5");
+  commentsCommentDate.classList.add("comments__comment-date");
+  commentsCommentDate.innerText = comment.date.toLocaleDateString("en-US");
+  commentsCommentTitle.appendChild(commentsCommentDate);
+
+  //adds the text of the comment
+  const commentsCommentText = document.createElement("p");
+  commentsCommentText.classList.add("comments__comment-text");
+  commentsCommentText.innerText = comment.comment;
+  commentFilledForm.appendChild(commentsCommentText);
+}
+
+loopThroughComments(comments);
+
+const commentForm = document.getElementById("commentForm");
+commentForm.addEventListener("submit", submitForm);
+
+function submitForm(event) {
+  event.preventDefault();
+  //   console.log("form was submitted");
+
+  let commentAuthor = event.target.commentAuthor.value;
+  let commentText = event.target.commentText.value;
+  let commentDate = new Date();
+
+  if (commentAuthor !== "" && commentText !== "") {
+    comments.push({
+      author: commentAuthor,
+      comment: commentText,
+      date: commentDate,
+    });
+    // console.log(comments);
+    // commentsSection.innerHTML = "";
+    const commentsBlock = document.getElementById("comments-output");
+    commentsBlock.innerHTML = "";
+    loopThroughComments(comments);
+    event.target.reset();
+  }
+}
---
+++ sprint-three/scripts/build-shows-page.js
@@ -0,0 +1,126 @@
+const showsSection = document.getElementById("showsSection");
+
+const shows = [
+  {
+    date: "Mon Dec 17 2018",
+    venue: "Ronald Lane",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Tue Jul 18 2018",
+    venue: "Pier 3 East",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Fri Jul 22 2019",
+    venue: "View Loungue",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Sat Aug 12 2019",
+    venue: "Hyatt Agency",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Fri Sep 05 2019",
+    venue: "Moscow Center",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Wed Aug 11 2019",
+    venue: "Pres Club",
+    location: "San Francisco, CA",
+  },
+];
+
+function loopThroughInfo(showsArr) {
+  for (let i = 0; i < showsArr.length; i++) {
+    // loops into the shows array
+    createShowElement(showsArr[i]); // calls the function with the loops to pass each object of the array
+  }
+}
+
+function createShowElement(show) {
+  const containerBlock = document.createElement("div"); // container to keep divs of dates, venues and locations
+  containerBlock.className = "container__block";
+
+  const containerBlockDate = document.createElement("div"); // container to store dates
+  containerBlockDate.className = "container__block-card";
+  const containerBlockVenue = document.createElement("div"); // container to store venues
+  containerBlockVenue.className = "container__block-card";
+  const containerBlockLocation = document.createElement("div"); // container to store locations
+  containerBlockLocation.className = "container__block-card";
+
+  const containerBlockButton = document.createElement("button"); // button
+  containerBlockButton.className = "container__block-button";
+  containerBlockButton.innerText = "BUY TICKETS";
+
+  const containerBlockDateTitle = document.createElement("h4"); // date title for each show
+  containerBlockDateTitle.className = "container__block-cardTitle";
+  containerBlockDateTitle.innerText = "DATE";
+  const containerBlockDateInput = document.createElement("p"); // stores dates for each show
+  containerBlockDateInput.className = "container__block-cardInput";
+  containerBlockDateInput.classList.add("container__block-cardDate");
+  containerBlockDateInput.innerText = show.date;
+
+  const containerBlockVenueTitle = document.createElement("h4"); // venue title for each show
+  containerBlockVenueTitle.className = "container__block-cardTitle";
+  containerBlockVenueTitle.innerText = "VENUE";
+  const containerBlockVenueInput = document.createElement("p"); // stores venues for each show
+  containerBlockVenueInput.className = "container__block-cardInput";
+  containerBlockVenueInput.innerText = show.venue;
+
+  const containerBlockLocationTitle = document.createElement("h4"); // location title for each show
+  containerBlockLocationTitle.className = "container__block-cardTitle";
+  containerBlockLocationTitle.innerText = "LOCATION";
+  const containerBlockLocationInput = document.createElement("p"); // stores locations for each show
+  containerBlockLocationInput.className = "container__block-cardInput";
+  containerBlockLocationInput.innerText = show.location;
+
+  showsContainer.appendChild(containerBlock); //appends container with dates, venues and locations subcontainer into a div
+
+  containerBlock.appendChild(containerBlockDate); // appends subcontainer with date title and input into one div
+  containerBlock.appendChild(containerBlockVenue); // appends subcontainer with venue title and input into one div
+  containerBlock.appendChild(containerBlockLocation); // appends subcontainer with location title and input into one div
+  containerBlock.appendChild(containerBlockButton); // appends button into a div container
+
+  containerBlockDate.appendChild(containerBlockDateTitle); // appends date title into the dates div
+  containerBlockDate.appendChild(containerBlockDateInput); // appends date input into the dates div
+
+  containerBlockVenue.appendChild(containerBlockVenueTitle); // appends venue title into the venues div
+  containerBlockVenue.appendChild(containerBlockVenueInput); // appends venue input into the venues div
+
+  containerBlockLocation.appendChild(containerBlockLocationTitle); // appends location title into the locations div
+  containerBlockLocation.appendChild(containerBlockLocationInput); // appends location input into the locations div
+
+  showsSection.appendChild(showsContainer); // appends main container into the shows section
+}
+
+const showsHeader = document.createElement("h1"); // section header
+showsHeader.className = "shows__header";
+showsHeader.innerText = "Shows";
+showsSection.appendChild(showsHeader);
+
+const showsTitleBlock = document.createElement("div"); // container to keep h3's
+showsTitleBlock.className = "shows__titleBlock";
+
+const showsTitleBlockDate = document.createElement("h3"); // table header - dates
+showsTitleBlockDate.className = "shows__titleBlock-heading";
+showsTitleBlockDate.classList.add("shows__titleBlockDate");
+showsTitleBlockDate.innerText = "DATES";
+showsTitleBlock.appendChild(showsTitleBlockDate);
+const showsTitleBlockVenue = document.createElement("h3"); // table header- venues
+showsTitleBlockVenue.className = "shows__titleBlock-heading";
+showsTitleBlockVenue.innerText = "VENUE";
+showsTitleBlock.appendChild(showsTitleBlockVenue);
+const showsTitleBlockLocation = document.createElement("h3"); // table header - locations
+showsTitleBlockLocation.className = "shows__titleBlock-heading";
+showsTitleBlockLocation.innerText = "LOCATION";
+showsTitleBlock.appendChild(showsTitleBlockLocation);
+
+const showsContainer = document.createElement("div"); // container to keep all data
+showsContainer.className = "shows__container";
+
+showsContainer.appendChild(showsTitleBlock); // appends div with h3 titles to the main container
+
+loopThroughInfo(shows); // calls the function passing the shows array
